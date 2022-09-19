47fbd0176e5f920fcff3eb4d725f395a9f2e5331
Dennis Nichols <dgnichols22@gmail.com>
2022-09-19 15:03:45 -0500

    added CSS styles for the sales and main pages

--- js/app.js
+++ js/app.js
@@ -1,36 +1,8 @@
-// Lab 06 Todo list
+// Create times array needed for generating lists later on
 
-// 1. Store the min/max hourly customers, and the average cookies per customer, in object properties.
+let times_array = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm'];
 
-// 2. Use a method of that object to generate a random number of customers per hour. Objects/Math/random
-
-// 3. Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated.
-
-// 4. Store the results for each location in a separate array… perhaps as a property of the object representing that location.
-
-// 5.  Display the values of each array as unordered lists in the browser.
-
-// 6.  Calculating the sum of these hourly totals; your output for each location should look like this:
-
-// Seattle
-
-// 6am: 16 cookies
-// 7am: 20 cookies
-// 8am: 35 cookies
-// 9am: 48 cookies
-// 10am: 56 cookies
-// 11am: 77 cookies
-// 12pm: 93 cookies
-// 1pm: 144 cookies
-// 2pm: 119 cookies
-// 3pm: 84 cookies
-// 4pm: 61 cookies
-// 5pm: 23 cookies
-// 6pm: 42 cookies
-// 7pm: 57 cookies
-// Total: 875 cookies
-
-// Create initial object format
+// Create store objects
 
 let seattle = {
 
@@ -52,7 +24,6 @@ let seattle = {
   }
 };
 
-// console.log(seattle.gen_sales_array());
 
 let tokyo = {
 
@@ -74,7 +45,73 @@ let tokyo = {
   }
 };
 
-let store_array = [seattle, tokyo];
+
+let dubai = {
+
+  location: 'Dubai',
+  min_hr: 11,
+  max_hr: 38,
+  avg_cookies: 3.7,
+  rand_cust_hr: function () {
+    let rand_cust = Math.floor(Math.random() * (this.max_hr - this.min_hr));
+    return rand_cust;
+  },
+  gen_sales_array: function () {
+    let sales_array = [];
+    for (let i = 0; i < 14; i++) {
+      let hour_sales = Math.floor(this.avg_cookies * this.rand_cust_hr());
+      sales_array.push(hour_sales);
+    }
+    return sales_array;
+  }
+};
+
+let paris = {
+
+  location: 'Paris',
+  min_hr: 20,
+  max_hr: 38,
+  avg_cookies: 2.3,
+  rand_cust_hr: function () {
+    let rand_cust = Math.floor(Math.random() * (this.max_hr - this.min_hr));
+    return rand_cust;
+  },
+  gen_sales_array: function () {
+    let sales_array = [];
+    for (let i = 0; i < 14; i++) {
+      let hour_sales = Math.floor(this.avg_cookies * this.rand_cust_hr());
+      sales_array.push(hour_sales);
+    }
+    return sales_array;
+  }
+};
+
+let lima = {
+
+  location: 'Lima',
+  min_hr: 2,
+  max_hr: 16,
+  avg_cookies: 4.6,
+  // Got the idea to add 1 to make this inclusive from the example in class which was in turn from the MDN docs
+  rand_cust_hr: function () {
+    let rand_cust = Math.floor(Math.random() * ((this.max_hr - this.min_hr)+ 1));
+    return rand_cust;
+  },
+  gen_sales_array: function () {
+    let sales_array = [];
+    for (let i = 0; i < 14; i++) {
+      let hour_sales = Math.floor(this.avg_cookies * this.rand_cust_hr());
+      sales_array.push(hour_sales);
+    }
+    return sales_array;
+  }
+};
+
+// Put store objects in an array
+
+let store_array = [seattle, tokyo, dubai, paris, lima];
+
+// Create function to display sales lists to the DOM
 
 const displaySales = function () {
 
@@ -91,11 +128,13 @@ const displaySales = function () {
 
     for (let i = 0; i < loc_sales.length; i++) {
       let list_el = document.createElement('li');
-      list_el.innerHTML = loc_sales[i] + ' cookies';
+      list_el.innerHTML = `${times_array[i]}: ${loc_sales[i]} cookies`;
       list.appendChild(list_el);
     }
   }
 };
+
+// Invoke function to display sales lists
 
 displaySales();
 
