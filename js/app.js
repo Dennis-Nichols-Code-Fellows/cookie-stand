d643a87232e4b966fe12079281495db16b398dc3
Dennis Nichols <dgnichols22@gmail.com>
2022-09-19 17:44:17 -0500

    added use strict to js script

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Create times array needed for generating lists later on
 
 let times_array = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
